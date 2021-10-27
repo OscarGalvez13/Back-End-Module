6cb78e7f2489dde6c19b442fda70ed493a72d06e
Oscar Galvez <oscar.galvez27@outlook.com>
2021-10-26 19:09:52 -0500

    categories router

--- rest_api/routes/categoriesRouter.js
+++ rest_api/routes/categoriesRouter.js
@@ -1,4 +1,80 @@
 const express = require("express");
 const router = express.Router();
+const faker = require("faker");
 
+router.get("/:id", (req, res) => {
+  id = req.params.id;
+  try {
+    res.json({
+      id,
+      categories: faker.commerce.department(),
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/", (req, res) => {
+  const categoriesCatalog = [];
+  const { limit } = req.query;
+  try {
+    for (let index = 0; index < limit; index++) {
+      categoriesCatalog.push({
+        categories: faker.commerce.department(),
+      });
+    }
+
+    if (limit) {
+      res.json({
+        categories: categoriesCatalog,
+      });
+    } else {
+      res.json({
+        ok: false,
+        message: "El limite es obligatorios",
+      });
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/", (req, res) => {
+  const body = req.body;
+  try {
+    res.json({
+      ok: true,
+      message: "Category added successfuly",
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.patch("/:id", (req, res) => {
+  const { id } = req.params;
+  const { category } = req.body;
+
+  try {
+    res.json({
+      ok: true,
+      message: `Category ${id} Update successfuly`,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  try {
+    //logica para eliminar
+    res.status(202).json({
+      ok: true,
+      message: `Category ${id} deleted successfuly`,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
 module.exports = router;
